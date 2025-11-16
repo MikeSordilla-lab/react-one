13801941ff17b4b7e4a88d1b452bfa699e729f92
Mike Sordilla <sordilla1@gmail.com>
2025-11-16 15:35:37 +0800

    refactor ListGroup component to manage selected item state and improve item click handling

--- react-one/src/components/ListGroup.tsx
+++ react-one/src/components/ListGroup.tsx
@@ -1,6 +1,7 @@
-import { MouseEvent } from "react";
+import { useState } from "react";
+
 function ListGroup() {
-  let items = [
+  const items = [
     "Tangub City",
     "Oroquieta City",
     "Ozamiz City",
@@ -8,15 +9,23 @@ function ListGroup() {
     "Cagayan de Oro City",
   ];
 
-  const handleClick = (event: MouseEvent) => console.log(event);
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
       <h1>List</h1>
       {items.length === 0 && <p>No items found. </p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li className="list-group-item" key={item} onClick={handleClick}>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => setSelectedIndex(index)}
+          >
             {item}
           </li>
         ))}
